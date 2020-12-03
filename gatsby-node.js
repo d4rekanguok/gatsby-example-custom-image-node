20b6d74cdb11e9a6a338ae5c43ec2b48ebb7b4a7
Derek Nguyen <derek@penandpillow.com>
2020-12-03 13:42:52 +0900

    fix

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,67 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
+const path = require('path')
+const workItems = require('./data/data.json')
 
-// You can delete this file if you're not using it
+// The convention is that type name must be title-cased.
+const WORK_NODE_NAME = 'WorkItem'
+
+exports.sourceNodes = ({ actions, createNodeId, createContentDigest }) => {
+    workItems.forEach((workItem) => {
+        const {
+            title,
+            images,
+        } = workItem
+
+        const node = {
+            id: createNodeId(`${WORK_NODE_NAME}>>>${title}`),
+            title,
+            images,
+            internal: {
+                type: WORK_NODE_NAME,
+                contentDigest: createContentDigest(workItem),
+            },
+        }
+
+        actions.createNode(node)
+    })
+}
+
+exports.createSchemaCustomization = ({ store, actions, schema }) => {
+    const { createTypes } = actions
+
+    // location of the working directory
+    const rootDir = store.getState().program.directory
+
+    // type name Gatsby gives to workItem.images
+    const IMAGE_NODE_NAME = WORK_NODE_NAME + 'Images'
+
+    /**
+     * Add a new field 'file' to WorkItemImages
+     * 
+     * You can also define the type in createSchemaCustomization
+     * then add this resolve function in createResolvers
+     * but I find it simpler to just do it here
+     */
+    const workImagesNode = schema.buildObjectType({
+        name: IMAGE_NODE_NAME,
+        fields: {
+            file: {
+                type: 'File!',
+                resolve: (src, _, context) => {
+                    // compose the absolute file path for image
+                    const absoluteImagePath = path.join(rootDir, src.url)
+                    return context.nodeModel
+                        .getAllNodes({ type: 'File' })
+                        .find(fileNode => {
+                            const { absolutePath } = fileNode
+                            return absoluteImagePath === absolutePath
+                        })
+                }
+            }
+        },
+        extensions: {
+            infer: true
+        }
+    })
+
+    createTypes([workImagesNode])
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,55 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+const IndexPage = ({ data }) => {
+  const { title, images } = data.workItem
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>{title}</h1>
+
+      <ul>
+        {images.map(({ title, alt, file }) => (
+          <li key={title}>
+            <article>
+            <h2>{title}</h2>
+            <pre style={{ whiteSpace: 'pre-line', fontSize: '1.25rem' }}>
+              title: {title}, {'\n'}
+              alt: {alt}, {'\n'}
+              src: {file.childImageSharp.fixed.src}
+            </pre>
+            <img src={file.childImageSharp.fixed.src} alt={alt} />
+            </article>
+          </li>
+        ))}
+      </ul>
+
+      <Link to="/page-2/">Go to page 2</Link> <br />
+      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    </Layout>
+  )
+}
 
 export default IndexPage
+
+export const query = graphql`
+  query IndexPage {
+    workItem {
+      title
+      images {
+        title
+        alt
+        file {
+          childImageSharp {
+            fixed {
+              src
+            }
+          }
+        }
+      }
+    }
+  }
+`
